9ee43dd79868a140eaabcc0c746069a24338f2e5
Matt Gordon <bigmattsize21@gmail.com>
2020-03-15 02:04:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash each do |season,holiday|
+holiday_hash.each do |season,holiday|
   puts "#{season.to_s.capitalize}:"
   holiday.each do |event,supplies|
     puts "  #{event.to_s.split("_").map { |x| x.capitalize }.join(" ") } :#{supplies.join(", ")}"
